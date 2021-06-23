0ff29b843ddcc5a3168ed240527b3a2fbb207452
BimbolaA <abimbolaabiri@gmail.com>
2021-06-23 10:11:53 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - a function that prints aplpahbet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+  int n;
+
+  for (n = 'a'; n <= 'z'; n++)
+    {
+      _putchar(n);
+    }
+  _putchar('\n');
+}
